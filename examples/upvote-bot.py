51ab65623d1c3e67e4c9d94067329b8e64d98f7c
PixelNoob <jesuschitty7@hotmail.com>
2016-07-07 08:45:55 -0300

    Waiting time
    Waiting time to vote added and log in verification

--- examples/upvote-bot.py
+++ examples/upvote-bot.py
@@ -1,11 +1,17 @@
+import time
 from piston.steem import Steem
 import os
 import json
+
 steem = Steem(wif=os.environ["WIF"])
+print ("login OK")
 authors = json.loads(os.environ["AUTHORS"])
+print ("checking authors OK")
 for c in steem.stream_comments(mode="head"):
      if c["author"] in authors and c.identifier == c.openingPostIdentifier:
         try:
+            time.sleep(1000)
             print(c.upvote())
         except:
-            print ("something went wrong, but your vote was casted and the bot its still running")
+            print ("something went wrong, your vote was NOT casted")
+
